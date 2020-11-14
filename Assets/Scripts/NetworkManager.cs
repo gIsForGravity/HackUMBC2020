de592622ea0b0bffbba882a0620e3807517b8d2b
Gravity <gavintantleff@gmail.com>
2020-11-13 22:41:14 -0500

    Add LiteNetLib

---
+++ Assets/Scripts/NetworkManager.cs
@@ -0,0 +1,52 @@
+﻿using LiteNetLib;
+using System.Net;
+using UnityEditor.PackageManager;
+using UnityEngine;
+
+public class NetworkManager : MonoBehaviour
+{
+    private bool ticking = false;
+    public int Tick { get; private set; } = 0;
+
+    private bool host = false;
+
+    NetManager netManager;
+    EventBasedNetListener netListener;
+
+    public void StartHost()
+    {
+        host = true;
+    }
+
+    public void StartClient()
+    {
+        netManager.Start();
+        netManager.Connect("localhost", 9050, "key");
+        ticking = true;
+    }
+
+    private void Start()
+    {
+        netListener = new EventBasedNetListener();
+        netListener.PeerConnectedEvent += (server) =>
+        {
+            Debug.LogError($"Connected to server: {server}");
+        };
+
+        netListener.ConnectionRequestEvent += (request) =>
+        {
+            request.Accept();
+        };
+
+        netManager = new NetManager(netListener);
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (!ticking) return;
+        Tick += 1;
+
+        netManager.PollEvents();
+    }
+}
