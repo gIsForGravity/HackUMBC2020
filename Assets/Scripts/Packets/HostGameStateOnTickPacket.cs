256756f4365d7cdf382b6867bc8c9bcf5522263d
Gravity <gavintantleff@gmail.com>
2020-11-15 00:03:14 -0500

    Another bodge, use a bunch of floats

--- Assets/Scripts/Packets/HostGameStateOnTickPacket.cs
+++ Assets/Scripts/Packets/HostGameStateOnTickPacket.cs
@@ -6,38 +6,65 @@ namespace HackUMBC
 {
     public class HostGameStateOnTickPacket : Packet
     {
-        public NetVector3[] ballLocations;
-        public NetQuaternion[] ballRotations;
-        public NetVector3 playerLocation;
-        public NetQuaternion playerRotation;
-        public NetVector3[] ballVelocities;
-        public NetVector3[] ballAngularVelocities;
-        public NetVector3 playerVelocity;
-        public NetVector3 playerAngularVelocity;
+        public float[] ballLocationsX;
+        public float[] ballLocationsY;
+        public float[] ballLocationsZ;
+
+        public float[] ballRotationsX;
+        public float[] ballRotationsY;
+        public float[] ballRotationsZ;
+        public float[] ballRotationsW;
+
+        public float playerLocationX;
+        public float playerLocationY;
+        public float playerLocationZ;
+
+        public float playerRotationX;
+        public float playerRotationY;
+        public float playerRotationZ;
+        public float playerRotationW;
+
+        public float[] ballVelocitiesX;
+        public float[] ballVelocitiesY;
+        public float[] ballVelocitiesZ;
+
+        public float[] ballAngularVelocitiesX;
+        public float[] ballAngularVelocitiesY;
+        public float[] ballAngularVelocitiesZ;
+
+        public float playerVelocityX;
+        public float playerVelocityY;
+        public float playerVelocityZ;
+
+        public float playerAngularVelocityX;
+        public float playerAngularVelocityY;
+        public float playerAngularVelocityZ;
+        public float playerAngularVelocityW;
+
         public int tick;
 
         public static GameState ToGameState(HostGameStateOnTickPacket packet)
         {
-            var length = packet.ballLocations.Length;
+            var length = packet.ballLocationsX.Length;
 
             GameState state = new GameState
             {
                 ballLocations = new Vector3[length],
                 ballRotations = new Quaternion[length],
-                playerLocation = packet.playerLocation,
-                playerRotation = packet.playerRotation,
+                playerLocation = new Vector3(packet.playerLocationX, packet.playerLocationY, packet.playerLocationZ),
+                playerRotation = new Quaternion(packet.playerRotationX, packet.playerRotationY, packet.playerRotationZ, packet.playerRotationW),
                 ballVelocities = new Vector3[length],
                 ballAngularVelocities = new Vector3[length],
-                playerVelocity = packet.playerVelocity,
-                playerAngularVelocity = packet.playerAngularVelocity
+                playerVelocity = new Vector3(packet.playerVelocityX, packet.playerVelocityY, packet.playerVelocityZ),
+                playerAngularVelocity = new Vector3(packet.playerAngularVelocityX, packet.playerAngularVelocityY, packet.playerAngularVelocityZ),
             };
 
-            for (int i = 0; i < packet.ballLocations.Length; i++)
+            for (int i = 0; i < packet.ballLocationsX.Length; i++)
             {
-                state.ballLocations[i] = packet.ballLocations[i];
-                state.ballRotations[i] = packet.ballRotations[i];
-                state.ballVelocities[i] = packet.ballVelocities[i];
-                state.ballAngularVelocities[i] = packet.ballAngularVelocities[i];
+                state.ballLocations[i] = new Vector3(packet.ballLocationsX[i], packet.ballLocationsY[i], packet.ballLocationsZ[i]);
+                state.ballRotations[i] = new Quaternion(packet.ballRotationsX[i], packet.ballRotationsY[i], packet.ballRotationsZ[i], packet.ballRotationsW[i]);
+                state.ballVelocities[i] = new Vector3(packet.ballVelocitiesX[i], packet.ballVelocitiesY[i], packet.ballVelocitiesZ[i]);
+                state.ballAngularVelocities[i] = new Vector3(packet.ballAngularVelocitiesX[i], packet.ballAngularVelocitiesX[i], packet.ballAngularVelocitiesX[i]);
             }
 
             return state;
@@ -49,23 +76,48 @@ namespace HackUMBC
 
             var packet = new HostGameStateOnTickPacket
             {
-                ballLocations = new NetVector3[length],
-                ballRotations = new NetQuaternion[length],
-                playerLocation = state.playerLocation,
-                playerRotation = state.playerRotation,
-                ballVelocities = new NetVector3[length],
-                ballAngularVelocities = new NetVector3[length],
-                playerVelocity = state.playerVelocity,
-                playerAngularVelocity = state.playerAngularVelocity,
+                ballLocationsX = new float[length],
+                ballLocationsY = new float[length],
+                ballLocationsZ = new float[length],
+                ballRotationsX = new float[length],
+                ballRotationsY = new float[length],
+                ballRotationsZ = new float[length],
+                ballRotationsW = new float[length],
+                playerLocationX = state.playerLocation.x,
+                playerLocationY = state.playerLocation.y,
+                playerLocationZ = state.playerLocation.z,
+                playerRotationX = state.playerRotation.x,
+                playerRotationY = state.playerRotation.y,
+                playerRotationZ = state.playerRotation.z,
+                ballVelocitiesX = new float[length],
+                ballVelocitiesY = new float[length],
+                ballVelocitiesZ = new float[length],
+                ballAngularVelocitiesX = new float[length],
+                ballAngularVelocitiesY = new float[length],
+                ballAngularVelocitiesZ = new float[length],
+                playerVelocityX = state.playerVelocity.x,
+                playerVelocityY = state.playerVelocity.y,
+                playerVelocityZ = state.playerVelocity.z,
+                playerAngularVelocityX = state.playerAngularVelocity.x,
+                playerAngularVelocityY = state.playerAngularVelocity.y,
+                playerAngularVelocityZ = state.playerAngularVelocity.z,
                 tick = tick
             };
 
-            for (int i = 0; i < packet.ballLocations.Length; i++)
+            for (int i = 0; i < length; i++)
             {
-                packet.ballLocations[i] = state.ballLocations[i];
-                packet.ballRotations[i] = state.ballRotations[i];
-                packet.ballVelocities[i] = state.ballVelocities[i];
-                packet.ballAngularVelocities[i] = state.ballAngularVelocities[i];
+                packet.ballLocationsX[i] = state.ballLocations[i].x;
+                packet.ballLocationsY[i] = state.ballLocations[i].y;
+                packet.ballLocationsZ[i] = state.ballLocations[i].z;
+                packet.ballRotationsX[i] = state.ballRotations[i].x;
+                packet.ballRotationsY[i] = state.ballRotations[i].y;
+                packet.ballRotationsZ[i] = state.ballRotations[i].z;
+                packet.ballVelocitiesX[i] = state.ballVelocities[i].x;
+                packet.ballVelocitiesY[i] = state.ballVelocities[i].y;
+                packet.ballVelocitiesZ[i] = state.ballVelocities[i].z;
+                packet.ballAngularVelocitiesX[i] = state.ballAngularVelocities[i].x;
+                packet.ballAngularVelocitiesY[i] = state.ballAngularVelocities[i].y;
+                packet.ballAngularVelocitiesZ[i] = state.ballAngularVelocities[i].z;
             }
 
             return packet;
